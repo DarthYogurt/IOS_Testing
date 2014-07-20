62db8cca934c85d4bebc88dea1679021fc096f99
Ray <raytochina@gmail.com>
2014-07-20 09:50:58 -0700

    xib done

--- Quiz/RWTQuizViewController.m
+++ Quiz/RWTQuizViewController.m
@@ -10,29 +10,50 @@
 
 @interface RWTQuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+
 @end
 
 @implementation RWTQuizViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (IBAction)showQuestion:(id)sender
 {
+    
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implemented by the superclass
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
     if (self) {
-        // Custom initialization
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
     }
+    
+    // Return the address of the new object
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
