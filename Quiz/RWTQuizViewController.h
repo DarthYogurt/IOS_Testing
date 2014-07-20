7f2358eb0e16f67fd2ac38fe2ef05bcf623fdd52
Ray <raytochina@gmail.com>
2014-07-20 04:36:50 -0700

    xib files edited

---
+++ Quiz/RWTQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWTQuizViewController.h
+//  Quiz
+//
+//  Created by Raymond Walintukan on 7/20/14.
+//  Copyright (c) 2014 Walintukai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTQuizViewController : UIViewController
+
+@end
---
+++ Quiz/RWTQuizViewController.m
@@ -0,0 +1,38 @@
+//
+//  RWTQuizViewController.m
+//  Quiz
+//
+//  Created by Raymond Walintukan on 7/20/14.
+//  Copyright (c) 2014 Walintukai. All rights reserved.
+//
+
+#import "RWTQuizViewController.h"
+
+@interface RWTQuizViewController ()
+
+@end
+
+@implementation RWTQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
